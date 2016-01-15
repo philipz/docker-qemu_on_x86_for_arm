fd4491345164598ec224bb2033c322849c18e35c
philipz <philipzheng@gmail.com>
2016-01-15 23:07:10 +0800

    Add demo4

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-#docker run -it --rm -v $(pwd)/qemu-arm-static:/usr/bin/qemu-arm-static -v $(pwd)/demo1:/app philipz/rpi-raspbian bash
-docker run -it --rm -v $(pwd)/qemu-arm-static:/usr/bin/qemu-arm-static -v $(pwd)/demo1:/app hypriot/rpi-java bash
+docker run -it --rm -v $(pwd)/qemu-arm-static:/usr/bin/qemu-arm-static -v $(pwd)/demo1:/app philipz/rpi-raspbian bash
+#docker run -it --rm -v $(pwd)/qemu-arm-static:/usr/bin/qemu-arm-static -v $(pwd)/demo1:/app hypriot/rpi-java bash
